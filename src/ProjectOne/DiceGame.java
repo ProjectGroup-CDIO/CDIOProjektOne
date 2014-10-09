da68879c3b2240d93e2b3ec1fe67f167dd46d747
THOMAS <thomas@liljegren.dk>
2014-10-09 12:09:40 +0200

    isPlayer forsøgt oprettet

--- src/ProjectOne/DiceGame.java
+++ src/ProjectOne/DiceGame.java
@@ -9,18 +9,18 @@ public class DiceGame {
 		System.out.println("This is a game, roll the dice if you are player one");
 		int playerOnePoints = 0;
 		int playerTwoPoints = 0; //Here each players points are stored as a variable int
-		
+
 		boolean playerOne = true;
 		boolean playerTwo = false; // these two variables are used to determine which players turn it is
-		
+
 		boolean Game = true; /*This variable is used to determine wether or not the game is 
 		still active*/
-		
+
 		Scanner CS = new Scanner(System.in); // the scanner is activated. 
-		
+
 		Die newDice = new Die(); //a new instance of the class Die is initiallized
 
-		
+
 		while(Game){
 			System.out.println("Write 'roll' to roll. \nWrite 'end' to end game and view score.");
 			String i = CS.nextLine();
@@ -29,32 +29,45 @@ public class DiceGame {
 				System.out.println(playerTwoPoints);
 				Game = false;
 				//break; 
-			}else if(i.equals("roll")){
-				System.out.println("rolling the dice");
-				int DiceOne = newDice.rollDice();
-				int DiceTwo = newDice.rollDice();
-				System.out.println("First Die: "+ DiceOne + " Second Die: " + DiceTwo);
-				if(playerOne){
-					playerOnePoints =playerOnePoints + DiceOne+DiceTwo;
-					playerTwo = true;
-					playerOne = false;
-					}else if(playerTwo){
-						playerTwoPoints = playerTwoPoints + DiceOne+DiceTwo;
-						playerOne = true;
-						playerTwo = false;
-						newDice.rollDice();
-				}
-				
+			}
+			if(playerOne){
+				System.out.println("Player ones turn.");
+			}if(playerTwo){
+				System.out.println("Player twos turn.");
+			}
+		}else if(i.equals("roll")){
+			System.out.println("rolling the dice");
+			int DiceOne = newDice.rollDice();
+			int DiceTwo = newDice.rollDice();
+			System.out.println("First Die: "+ DiceOne + " Second Die: " + DiceTwo);
+			if(playerOne){
+				playerOnePoints = playerOnePoints + DiceOne+DiceTwo;
+				playerTwo = true;
+				playerOne = false;
+			}else if(playerTwo){
+				playerTwoPoints = playerTwoPoints + DiceOne+DiceTwo;
+				playerOne = true;
+				playerTwo = false;
+				newDice.rollDice();
+			}
+
 			System.out.println("playerOnePoints: "+ playerOnePoints);
 			System.out.println("playerTwoPoints: "+ playerTwoPoints);
+		}
+		else{
+			System.out.println("Not a valid input! Either roll or end.");
+		}
+
+
+		public void isPlayer(){
+			if(playerOne){
+				System.out.println("Player ones turn.");
+			}if(playerTwo){
+				System.out.println("Player twos turn.");
 			}
-			else{
-				System.out.println("Not a valid input! Either roll or end.");
-			}
-		
 		}
 	}
-
 }
-	
+
+
 
