c2258a8d239fed4ff4fabfb86bc4f4f128f6fbb6
Martin Vieth <martin_123_vieth@hotmail.com>
2014-10-16 13:26:42 +0200

    Gave up and made a test for the dice roller

--- src/ProjectOne/Tests.java
+++ src/ProjectOne/Tests.java
@@ -4,31 +4,43 @@ public class Tests {
 
 	public static void main(String[] args) {
 		
-		DiceGame newgame = new DiceGame();
 		
+		Die newDice = new Die();
 		
+		int ones = 0;
+		int twoes = 0;
+		int threes = 0;
+		int fours = 0;
+		int fivers = 0;
+		int sixers = 0;
 		
-		//test one double pair 6
-		int[] array1 = {2,2,6,6,4,2};
-		int[] array2 = {3,2,6,6,3,3};
-		for(int i = 0; i<array1.length;i++){
-		newgame.gameLogic("roll", array1[i], array2[i]);
-		if(!newgame.Game){
-			break;
+		for(int i = 0; i < 10000000; i++){
+			int a =newDice.rollDice();
+			
+			if(a == 1){
+				ones = ones + 1;
+			}
+			if(a == 2){
+				twoes= twoes + 1;
+			}
+			if(a == 3){
+				threes = threes + 1;
+			}
+			if(a == 4){
+				fours = fours + 1;
+			}
+			if(a == 5){
+				fivers = fivers + 1;
+			}
+			if(a == 6){
+				sixers = sixers + 1;
+			}
 		}
-		}
-		newgame.gameEndCheck();
-		System.out.println("Test one completed expected player to win - confirmed?");
-		
-		
-
-		/*
-		newgame.diceChecks(6, 6);
-		System.out.println("playerOnePoints: "+ newgame.playerOnePoints);
-		System.out.println("playerTwoPoints: "+ newgame.playerTwoPoints);
-		newgame.diceChecks(6, 6);
-		System.out.println("playerOnePoints: "+ newgame.playerOnePoints);
-		System.out.println("playerTwoPoints: "+ newgame.playerTwoPoints);
-		*/
+		System.out.println("Nr of 1 rolled: " +ones);
+		System.out.println("Nr of 2 rolled: "+twoes);
+		System.out.println("Nr of 3 rolled: "+threes);
+		System.out.println("Nr of 4 rolled: "+fours);
+		System.out.println("Nr of 5 rolled: "+fivers);
+		System.out.println("Nr of 6 rolled: "+sixers);
 	}
 }
