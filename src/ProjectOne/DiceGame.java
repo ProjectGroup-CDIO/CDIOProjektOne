f734c1dcc770a50cdec777a801c11efad91471e6
Kenneth Frandsen <Frandsen86@gmail.com>
2014-10-16 13:29:41 +0200

    merged of rettet.

--- src/ProjectOne/DiceGame.java
+++ src/ProjectOne/DiceGame.java
@@ -1,6 +1,6 @@
 package ProjectOne;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 import boundaryToMatador.GUI;
 
@@ -12,21 +12,23 @@ public class DiceGame {
 	static boolean playerOne = true;
 	static boolean playerTwo = false; // these two variables are used to determine which players turn it is
 
+
 	static boolean chanceToWinOne = false;
 	static boolean chanceToWinTwo = false;
-	
+
 	static boolean confirmVicOne = false;
 	static boolean confirmVicTwo = false;
-	
+
 	static boolean wasLastRollDoubleSix = false;
 
-	static boolean Game = true; /*This variable is used to determine whether or not the game is 
-	still active*/
+	static boolean Game = true; //This variable is used to determine whether or not the game is still active
+
 
 	public static void main(String[] args) {
-		System.out.println("This is a game, roll the dice if you are player one");
+		System.out.println("This is a game, roll the dice if you are Player One");
 
-		Scanner CS = new Scanner(System.in); // the scanner is activated for TUI
+
+		//Scanner CS = new Scanner(System.in); // the scanner is activated for TUI
 
 		Die newDice = new Die(); //a new instance of the class Die is initialized
 		
@@ -34,14 +36,18 @@ public class DiceGame {
 		GUI.addPlayer("Player Two", playerTwoPoints);
 		GUI.addPlayer("Player One", playerOnePoints);
 
-		//int z = 0;
+		GUI.addPlayer("Player Two", playerOnePoints); // Opretter spiller 2 på brættet
+		GUI.addPlayer("Player One", playerOnePoints); // Oprætter spiller 1 på brættet
 		
+
 		while(Game){
+
 			System.out.println("Write 'roll' to roll. \nWrite 'end' to end game and view score.");
 			String i = "";
 			i = GUI.getUserButtonPressed(null, "Roll Dice"); // used for GUI. Returns the string "Roll Dice"
 			isPlayer(playerOne,playerTwo);
 			//i = CS.nextLine(); // used for TUI
+
 
 			if(i.equals("end")){
 				System.out.println(playerOnePoints);
@@ -54,13 +60,16 @@ public class DiceGame {
 				int diceOne = newDice.rollDice(); //integer is created to represent faceValue of die
 				int diceTwo = newDice.rollDice();
 				GUI.setDice(diceOne, diceTwo);
+
 				/*
 				int[] array1 = {2,2,6,4,5,5,5,5,5,5,5,5,5};
 				int[] array2 = {3,2,6,6,4,4,4,4,4,4,4,4,4};
 				int DiceOne = array1[z];
 				int DiceTwo = array2[z];
+<<<<<<< HEAD
 				*/
 				System.out.println("First Die: "+ diceOne + " Second Die: " + diceTwo);
+
 
 				if(playerOne){
 					if(diceOne == diceTwo && playerOnePoints >= 40 && diceOne!=1){
@@ -84,8 +93,10 @@ public class DiceGame {
 						isPair(diceOne, diceTwo);
 					}
 					else{
+
 						playerOnePoints = playerOnePoints + diceOne+diceTwo;
 						GUI.setBalance("Player One", playerOnePoints);
+
 					}
 					if(diceOne == diceTwo && diceOne == 6){
 						if(chanceToWinOne){
@@ -100,6 +111,7 @@ public class DiceGame {
 							chanceToWinOne = true;
 							isPair(diceOne, diceTwo);
 						}
+
 					}
 				}
 				else if(playerTwo){
@@ -144,43 +156,54 @@ public class DiceGame {
 						}
 					}
 				}
-				System.out.println("playerOnePoints: "+ playerOnePoints);
-				System.out.println("playerTwoPoints: "+ playerTwoPoints);
+				System.out.println("Player One Points: "+ playerOnePoints);
+				System.out.println("Player Two Points: "+ playerTwoPoints);
 			}
 			else{
-				System.out.println("Not a valid input! Either roll or end.");
+				System.out.println("Not a valid input! Either r or end.");
 			}
-		//z++;
+			//z++;
 		}
-		
+
 		if (Game != true)
 		{
 			System.out.println("Resolving which player which wins");
 			if( confirmVicOne  && !confirmVicTwo || (playerTwoPoints < playerOnePoints && confirmVicOne))
 			{
 				System.out.println("PlayerOne Won");
+				GUI.showMessage("PlayerOne Won");
+				GUI.addPlayer("Player One is the Winner!!!!", playerOnePoints);
+				
 			}
 			if((confirmVicTwo && !confirmVicOne ) || (playerOnePoints < playerTwoPoints && confirmVicTwo))
 			{
 				System.out.println("PlayerTwo Won");
+				GUI.showMessage("PlayerTwo Won");
+				GUI.addPlayer("Player Two is the Winner!!!!", playerOnePoints);
 			}
 			if(playerOnePoints == playerTwoPoints && confirmVicOne == true && confirmVicTwo == true )
 			{
 				System.out.println("Draw");
+				GUI.showMessage("Draw!!");
+				GUI.addPlayer("The game was a Draw!!", playerOnePoints);
 			}
-			
+
 		}
 	}
-
+		
 
 
 
 	public static void isPlayer(boolean a, boolean b){
 		if(a){
 			System.out.println("Player ones turn.");
+			GUI.showMessage("Player ones turn");
+			
 		}
 		if(b){
 			System.out.println("Player twos turn.");
+			GUI.showMessage("Player twos turn");
+			
 		}
 	}
 
@@ -191,13 +214,13 @@ public class DiceGame {
 				playerOne = true;
 			playerTwo = false;
 		}
-		
+
 		if(playerTwo){
 			if(a == b){
 				playerTwo = true;
 				playerOne = false;
 			}
-		
+
 		}
 
 	}
