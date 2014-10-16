f18968f1c4a7abf927547a2ef4d15823eab9ad5d
Delvaux Paulin Niyonzima <s144849@student.dtu.dk>
2014-10-16 14:58:19 +0200

    Tilføjer timer

--- src/ProjectOne/DiceGame.java
+++ src/ProjectOne/DiceGame.java
@@ -19,20 +19,21 @@ public class DiceGame {
 	static boolean confirmVicOne = false;
 	static boolean confirmVicTwo = false;
 
+	static boolean firstRollOne = true;
+	static boolean firstRollTwo = true;
+	
 	static boolean wasLastRollDoubleSix = false;
 
 	static boolean game = true; //This variable is used to determine whether or not the game is still active
 
+	static long duration = 0;
 
 	public static void main(String[] args) {
 		System.out.println("This is a game, roll the dice if you are Player One");
-
-
 		//Scanner CS = new Scanner(System.in); // the scanner is activated for TUI
 
 		Die newDice = new Die(); //a new instance of the class Die is initialized
-		
-
+	
 		GUI.addPlayer("Player Two", playerTwoPoints); // Opretter spiller 2 på brættet
 		GUI.addPlayer("Player One", playerOnePoints); // Opretter spiller 1 på brættet
 		
@@ -44,126 +45,21 @@ public class DiceGame {
 			i = GUI.getUserButtonPressed(null, "Roll Dice"); // used for GUI. Returns the string "Roll Dice"
 			isPlayer(playerOne,playerTwo);
 			//i = CS.nextLine(); // used for TUI
+			int diceOne = newDice.rollDice(); //integer is created to represent faceValue of die
+			int diceTwo = newDice.rollDice();
 
-
-			if(i.equals("end")){
-				System.out.println(playerOnePoints);
-				System.out.println(playerTwoPoints);
-				game = false;
-				//break; 
-
-			}else if(i.equals("Roll Dice")){
-				System.out.println("rolling the dice");
-				int diceOne = newDice.rollDice(); //integer is created to represent faceValue of die
-				int diceTwo = newDice.rollDice();
-				GUI.setDice(diceOne, diceTwo);
-
-				/*
-				int[] array1 = {2,2,6,4,5,5,5,5,5,5,5,5,5};
-				int[] array2 = {3,2,6,6,4,4,4,4,4,4,4,4,4};
-				int DiceOne = array1[z];
-				int DiceTwo = array2[z];
-<<<<<<< HEAD
-				*/
-				System.out.println("First Die: "+ diceOne + " Second Die: " + diceTwo);
-
-
-				if(playerOne){
-					if(diceOne == diceTwo && playerOnePoints >= 40 && diceOne!=1){
-						confirmVicOne = true;
-						isPair(diceOne, diceTwo); //isPair is called to give player another turn
-					}
-					else if(diceOne == diceTwo && playerOnePoints < 40){
-						isPair(diceOne, diceTwo);
-					}
-					else{
-						if (confirmVicTwo){
-							game = false;
-						}
-						playerTwo = true;
-						playerOne = false;
-					}
-
-					if(diceOne == diceTwo && diceOne == 1){
-						confirmVicOne = false;
-						playerOnePoints = 0;
-						playerOne = true;
-						playerTwo = false;
-					}
-					else{
-
-						playerOnePoints = playerOnePoints + diceOne+diceTwo;
-						GUI.setBalance("Player One", playerOnePoints);
-
-					}
-					if(diceOne == diceTwo && diceOne == 6){
-						if(chanceToWinOne){
-							confirmVicOne = true;
-							playerTwo = true;
-							playerOne = false;
-							if (confirmVicTwo == true){
-								game = false;
-							}
-						}
-						else{
-							chanceToWinOne = true;
-							isPair(diceOne, diceTwo);
-						}
-
-					}
-				}
-				else if(playerTwo){
-					if(diceOne == diceTwo && playerTwoPoints >= 40 && diceOne!=1){
-						confirmVicTwo = true;
-						isPair(diceOne, diceTwo);
-					}
-					else if(diceOne == diceTwo && playerTwoPoints < 40){
-						isPair(diceOne, diceTwo);
-					}
-					else{
-						if (confirmVicOne){
-							game = false;
-						}
-						playerOne = true;
-						playerTwo = false;
-					}
-
-					if(diceOne == diceTwo && diceOne == 1){
-						playerTwoPoints = 0;
-						confirmVicTwo = false;
-						playerOne = false;
-						playerTwo = true;
-					}
-					else{
-						playerTwoPoints = playerTwoPoints + diceOne+diceTwo;
-						GUI.setBalance("Player Two", playerTwoPoints);
-
-					}
-					if(diceOne == diceTwo && diceOne == 6){
-						if(chanceToWinTwo){
-							confirmVicTwo = true;
-							if (confirmVicOne == true){
-								game = false;
-							}
-							playerTwo = false;
-							playerOne = true;
-						}
-						else
-						{
-							chanceToWinTwo = true;
-							isPair(diceOne, diceTwo);
-						}
-					}
-				}
-				System.out.println("Player One Points: "+ playerOnePoints);
-				System.out.println("Player Two Points: "+ playerTwoPoints);
-			}
-			else{
-				System.out.println("Not a valid input! Either r or end.");
-			}
+			gameLogic(i, diceOne, diceTwo);
 			//z++;
 		}
 
+		checkForWinner();
+	}
+
+
+
+
+	private static void checkForWinner() {
+		long startTime = System.currentTimeMillis();
 		if (game != true)
 		{
 			System.out.println("Resolving which player which wins");
@@ -188,6 +84,140 @@ public class DiceGame {
 			}
 
 		}
+		long endTime = System.currentTimeMillis();
+		duration = duration + (endTime - startTime);
+		System.out.println("That took " + duration + " milliseconds");
+	}
+
+
+
+
+	private static void gameLogic(String i, int diceOne, int diceTwo) {
+		long startTime = System.currentTimeMillis();
+		if(i.equals("end")){
+			System.out.println(playerOnePoints);
+			System.out.println(playerTwoPoints);
+			game = false;
+			//break; 
+
+		}else if(i.equals("Roll Dice")){
+			System.out.println("rolling the dice");
+			
+			GUI.setDice(diceOne, diceTwo);
+
+			/*
+			int[] array1 = {2,2,6,4,5,5,5,5,5,5,5,5,5};
+			int[] array2 = {3,2,6,6,4,4,4,4,4,4,4,4,4};
+			int DiceOne = array1[z];
+			int DiceTwo = array2[z];
+<<<<<<< HEAD
+			*/
+			System.out.println("First Die: "+ diceOne + " Second Die: " + diceTwo);
+
+
+			if(playerOne){
+				if(diceOne == diceTwo && playerOnePoints >= 40 && diceOne!=1){
+					if (firstRollOne){
+					confirmVicOne = true;
+					}
+					
+					isPair(diceOne, diceTwo); //isPair is called to give player another turn
+				}
+				else if(diceOne == diceTwo && playerOnePoints < 40){
+					isPair(diceOne, diceTwo);
+				}
+				else{
+					if (confirmVicTwo){
+						game = false;
+					}
+					playerTwo = true;
+					playerOne = false;
+				}
+
+				if(diceOne == diceTwo && diceOne == 1){
+					confirmVicOne = false;
+					playerOnePoints = 0;
+					playerOne = true;
+					playerTwo = false;
+				}
+				else{
+
+					playerOnePoints = playerOnePoints + diceOne+diceTwo;
+					GUI.setBalance("Player One", playerOnePoints);
+
+				}
+				if(diceOne == diceTwo && diceOne == 6){
+					if(chanceToWinOne){
+						confirmVicOne = true;
+						playerTwo = true;
+						playerOne = false;
+						if (confirmVicTwo == true){
+							game = false;
+						}
+					}
+					else{
+						chanceToWinOne = true;
+						isPair(diceOne, diceTwo);
+					}
+
+				}
+				firstRollTwo = true;
+			}
+			else if(playerTwo){
+				if(diceOne == diceTwo && playerTwoPoints >= 40 && diceOne!=1){
+					if (firstRollTwo){
+					confirmVicTwo = true;
+					}
+					isPair(diceOne, diceTwo);
+				}
+				else if(diceOne == diceTwo && playerTwoPoints < 40){
+					isPair(diceOne, diceTwo);
+				}
+				else{
+					if (confirmVicOne){
+						game = false;
+					}
+					playerOne = true;
+					playerTwo = false;
+				}
+
+				if(diceOne == diceTwo && diceOne == 1){
+					playerTwoPoints = 0;
+					confirmVicTwo = false;
+					playerOne = false;
+					playerTwo = true;
+				}
+				else{
+					playerTwoPoints = playerTwoPoints + diceOne+diceTwo;
+					GUI.setBalance("Player Two", playerTwoPoints);
+
+				}
+				if(diceOne == diceTwo && diceOne == 6){
+					if(chanceToWinTwo){
+						confirmVicTwo = true;
+						if (confirmVicOne == true){
+							game = false;
+						}
+						playerTwo = false;
+						playerOne = true;
+					}
+					else
+					{
+						chanceToWinTwo = true;
+						isPair(diceOne, diceTwo);
+					}
+				}
+				firstRollOne = true;
+			}
+			System.out.println("Player One Points: "+ playerOnePoints);
+			System.out.println("Player Two Points: "+ playerTwoPoints);
+		}
+		else{
+			System.out.println("Not a valid input! Either r or end.");
+		}
+		long endTime = System.currentTimeMillis();
+		duration = endTime - startTime;
+		System.out.println("That took " + duration + " milliseconds");
 	}
 		
 
