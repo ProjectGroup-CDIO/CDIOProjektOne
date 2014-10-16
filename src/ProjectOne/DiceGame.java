114af7a9fee32a612fbe9f660f9bee2ad6cda165
Martin Vieth <martin_123_vieth@hotmail.com>
2014-10-16 10:54:29 +0200

    Synched with master branch

--- src/ProjectOne/DiceGame.java
+++ src/ProjectOne/DiceGame.java
@@ -3,21 +3,23 @@ package ProjectOne;
 import java.util.Scanner;
 
 public class DiceGame {
+
 	static int playerOnePoints = 0;
 	static int playerTwoPoints = 0; //Here each players points are stored as a variable int
 
 	static boolean playerOne = true;
 	static boolean playerTwo = false; // these two variables are used to determine which players turn it is
 
-	static boolean winOne = false;
-	static boolean winTwo = false; // booleans for determining if player one and two has completed the game
-
+	static boolean chanceToWinOne = false;
+	static boolean chanceToWinTwo = false;
+	
+	static boolean confirmVicOne = false;
+	static boolean confirmVicTwo = false;
+	
 	static boolean wasLastRollDoubleSix = false;
 
 	static boolean Game = true; /*This variable is used to determine whether or not the game is 
 	still active*/
-
-
 
 	public static void main(String[] args) {
 		System.out.println("This is a game, roll the dice if you are player one");
@@ -26,7 +28,8 @@ public class DiceGame {
 
 		Die newDice = new Die(); //a new instance of the class Die is initialized
 
-
+		//int z = 0;
+		
 		while(Game){
 			System.out.println("Write 'roll' to roll. \nWrite 'end' to end game and view score.");
 			isPlayer(playerOne,playerTwo);
@@ -39,72 +42,124 @@ public class DiceGame {
 				//break; 
 
 			}else if(i.equals("roll")){
-				
+				System.out.println("rolling the dice");
 				int DiceOne = newDice.rollDice();
 				int DiceTwo = newDice.rollDice();
-				diceChecks(DiceOne, DiceTwo);
-				winCheckOneAndTwo();
+				/*
+				int[] array1 = {2,2,6,4,5,5,5,5,5,5,5,5,5};
+				int[] array2 = {3,2,6,6,4,4,4,4,4,4,4,4,4};
+				int DiceOne = array1[z];
+				int DiceTwo = array2[z];
+				*/
+				System.out.println("First Die: "+ DiceOne + " Second Die: " + DiceTwo);
+
+				if(playerOne){
+					if(DiceOne == DiceTwo && playerOnePoints >= 40 && DiceOne!=1){
+						confirmVicOne = true;
+						isPair(DiceOne, DiceTwo);
+					}
+					else if(DiceOne == DiceTwo && playerOnePoints < 40){
+						isPair(DiceOne, DiceTwo);
+					}
+					else{
+						if (confirmVicTwo){
+							Game = false;
+						}
+						playerTwo = true;
+						playerOne = false;
+					}
+
+					if(DiceOne == DiceTwo && DiceOne == 1){
+						confirmVicOne = false;
+						playerOnePoints = 0;
+					}
+					else{
+						playerOnePoints = playerOnePoints + DiceOne+DiceTwo;
+					}
+					if(DiceOne == DiceTwo && DiceOne == 6){
+						if(chanceToWinOne){
+							confirmVicOne = true;
+							playerTwo = true;
+							playerOne = false;
+							if (confirmVicTwo == true){
+								Game = false;
+							}
+						}
+						else{
+							chanceToWinOne = true;
+							isPair(DiceOne, DiceTwo);
+						}
+					}
+				}
+				else if(playerTwo){
+					if(DiceOne == DiceTwo && playerTwoPoints >= 40 && DiceOne!=1){
+						confirmVicTwo = true;
+						isPair(DiceOne, DiceTwo);
+					}
+					else if(DiceOne == DiceTwo && playerTwoPoints < 40){
+						isPair(DiceOne, DiceTwo);
+					}
+					else{
+						if (confirmVicOne){
+							Game = false;
+						}
+						playerOne = true;
+						playerTwo = false;
+					}
+
+					if(DiceOne == DiceTwo && DiceOne == 1){
+						playerTwoPoints = 0;
+						confirmVicTwo = false;
+					}else{
+						playerTwoPoints = playerTwoPoints + DiceOne+DiceTwo;
+
+					}
+					if(DiceOne == DiceTwo && DiceOne == 6){
+						if(chanceToWinTwo){
+							confirmVicTwo = true;
+							if (confirmVicOne == true){
+								Game = false;
+							}
+							playerTwo = false;
+							playerOne = true;
+						}
+						else
+						{
+							chanceToWinTwo = true;
+							isPair(DiceOne, DiceTwo);
+						}
+					}
+				}
+				System.out.println("playerOnePoints: "+ playerOnePoints);
+				System.out.println("playerTwoPoints: "+ playerTwoPoints);
 			}
 			else{
 				System.out.println("Not a valid input! Either roll or end.");
 			}
+		//z++;
+		}
+		
+		if (Game != true)
+		{
+			System.out.println("Resolving which player which wins");
+			if( confirmVicOne  && !confirmVicTwo || (playerTwoPoints < playerOnePoints && confirmVicOne))
+			{
+				System.out.println("PlayerOne Won");
+			}
+			if((confirmVicTwo && !confirmVicOne ) || (playerOnePoints < playerTwoPoints && confirmVicTwo))
+			{
+				System.out.println("PlayerTwo Won");
+			}
+			if(playerOnePoints == playerTwoPoints && confirmVicOne == true && confirmVicTwo == true )
+			{
+				System.out.println("Draw");
+			}
+			
 		}
 	}
 
-	public static void winCheckOneAndTwo() {
-		if(winOne && winTwo){
-			System.out.println("its a draw! now checking each players points");
-			//method for comparing players points and output who won
-			Game = false;
-		}else if( winTwo){
-			System.out.println("Player two has won!");
-			Game = false;
-		}else if(winOne){
-			System.out.println("Player one has won!");
-			Game = false;
-		}
-		System.out.println("playerOnePoints: "+ playerOnePoints);
-		System.out.println("playerTwoPoints: "+ playerTwoPoints);
-	}
 
-	public static void diceChecks(int DiceOne, int DiceTwo) {
-		System.out.println("rolling the dice");
-		System.out.println("First Die: "+ DiceOne + " Second Die: " + DiceTwo);
-		if(playerOne){
-			if(DiceOne == DiceTwo){
-				isPair(DiceOne, DiceTwo);
-			}else{
-				playerTwo = true;
-				playerOne = false;
-			}
-			if(DiceOne == DiceTwo && DiceOne == 1){
-				playerOnePoints = 0;
-			}else{
-				playerOnePoints = playerOnePoints + DiceOne+DiceTwo;
-			}
-			if(DiceOne != DiceTwo){
-				wasLastRollDoubleSix = false;
-			}
-		}
-		else if(playerTwo){
-			if(DiceOne == DiceTwo){
-				isPair(DiceOne, DiceTwo);
-			}else{
-				playerOne = true;
-				playerTwo = false;
-			}
-			if(DiceOne == DiceTwo && DiceOne == 1){
-				playerTwoPoints = 0;
-			}else{
-				playerTwoPoints = playerTwoPoints + DiceOne+DiceTwo;
 
-			}
-			if(DiceOne != DiceTwo){
-				wasLastRollDoubleSix = false;
-
-			}
-		}
-	}
 
 	public static void isPlayer(boolean a, boolean b){
 		if(a){
@@ -117,40 +172,23 @@ public class DiceGame {
 
 
 	public static void isPair(int a, int b){
-		
 		if(playerOne){
 			if(a == b)
 				playerOne = true;
 			playerTwo = false;
 		}
-		 if(a == 6){
-			if(wasLastRollDoubleSix){
-				System.out.println("Player One has won!!!!!1111ELEVEN");
-				winOne = true;
-
-			}
-			wasLastRollDoubleSix = true;
-		}
+		
 		if(playerTwo){
 			if(a == b){
 				playerTwo = true;
 				playerOne = false;
 			}
-
-			if( a == 6){
-				if(wasLastRollDoubleSix){
-					System.out.println("Player One has won!!!!!1111ELEVEN");
-					winTwo = true;
-
-				}
-				wasLastRollDoubleSix = true;
-			}
+		
 		}
 
 	}
 
 }
-
 
 
 
--- src/ProjectOne/Tests.java
+++ src/ProjectOne/Tests.java
@@ -6,12 +6,13 @@ public class Tests {
 		
 		DiceGame newgame = new DiceGame();
 		
-		
+		/*
 		newgame.diceChecks(6, 6);
 		System.out.println("playerOnePoints: "+ newgame.playerOnePoints);
 		System.out.println("playerTwoPoints: "+ newgame.playerTwoPoints);
 		newgame.diceChecks(6, 6);
 		System.out.println("playerOnePoints: "+ newgame.playerOnePoints);
 		System.out.println("playerTwoPoints: "+ newgame.playerTwoPoints);
+		*/
 	}
 }
